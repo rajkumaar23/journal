35c71b836913205831610eaddf6a4b48ca9ca38d
Rajkumar S <rajkumaar2304@icloud.com>
2021-05-20 18:50:41 +0530

    Set primary key in model

--- app/Models/JournalEntry.php
+++ app/Models/JournalEntry.php
@@ -8,10 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @method static where(string $string, $date)
  * @method static updateOrInsert(array $array, array $array1)
+ * @method static updateOrCreate(array $array, array $array1)
+ * @method static create(array $array)
  */
 class JournalEntry extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'date';
 
     /**
      * The attributes that are mass assignable.

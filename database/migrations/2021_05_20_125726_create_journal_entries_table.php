97b3fcc12d29d372bc7b2ba3c3adff6b3f790df0
Rajkumar S <rajkumaar2304@icloud.com>
2021-05-20 18:28:06 +0530

    Add migration for creating diary entries table

---
+++ database/migrations/2021_05_20_125726_create_journal_entries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJournalEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journal_entries', function (Blueprint $table) {
+            $table->date('date')->primary();
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journal_entries');
+    }
+}

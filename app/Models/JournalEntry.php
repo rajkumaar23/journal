4e1847035ec740cf15b64e43f46f95293d9f609a
Rajkumar S <rajkumaar2304@icloud.com>
2021-05-20 18:30:55 +0530

    Add fillable fields

--- app/Models/JournalEntry.php
+++ app/Models/JournalEntry.php
@@ -5,7 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static where(string $string, $date)
+ * @method static updateOrInsert(array $array, array $array1)
+ */
 class JournalEntry extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'date', 'body',
+    ];
 }

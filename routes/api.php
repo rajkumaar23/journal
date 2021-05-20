45443f0ba9c36cd654cad1b1e80186b15dbe61c7
Rajkumar S <rajkumaar2304@icloud.com>
2021-05-20 18:32:32 +0530

    Add API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\JournalEntryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('entries/{date}', [JournalEntryController::class, 'getEntries']);
+Route::post('entries/{date}', [JournalEntryController::class, 'addOrUpdateEntry']);
+
